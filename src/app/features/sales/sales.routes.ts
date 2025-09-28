1a5742cb52c4deb9a6cca4c18798af1ee7100793
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-09-28 19:40:11 +0100

    sales

--- src/app/features/sales/sales.routes.ts
+++ src/app/features/sales/sales.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
-// import { PointOfSaleComponent } from './pages/point-of-sale/point-of-sale.component';
-// import { SalesHistoryComponent } from './pages/sales-history/sales-history.component';
+import { SalesHistoryComponent } from './pages/sales-history/sales-history.component';
 
 export const SALES_ROUTES: Routes = [
   {
     path: '',
-    redirectTo: 'pos',
+    redirectTo: 'history',
     pathMatch: 'full',
   },
   //   {
   //     path: 'pos',
   //     component: PointOfSaleComponent,
   //   },
-  //   {
-  //     path: 'history',
-  //     component: SalesHistoryComponent,
-  //   },
+  {
+    path: 'history',
+    component: SalesHistoryComponent,
+  },
 ];
