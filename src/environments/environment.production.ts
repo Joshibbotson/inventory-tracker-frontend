86e0761d4652348a0b0a22ffaaeb98fdadb88aa8
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-09-28 10:24:58 +0100

    url fix

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'inventory-tracker-backend-production.up.railway.app',
+  apiUrl: 'https://inventory-tracker-backend-production.up.railway.app',
 };
